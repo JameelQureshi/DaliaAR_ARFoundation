7291f3bed55812c8a2a3094e501555e886ec2100
Jameel Qureshi <jameelqureshi2013@gmail.com>
2021-03-23 14:40:43 +0500

    VideoUpdate

--- Assets/Apregon/TriggerEvent.cs
+++ Assets/Apregon/TriggerEvent.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class TriggerEvent : MonoBehaviour
 {
     public Transform target;
+    public Material material;
+
 
     private void Start()
     {
@@ -14,17 +16,25 @@ public class TriggerEvent : MonoBehaviour
     private void Update()
     {
         float dist = Vector3.Distance(target.position, transform.position);
-        //print("Distance to target: " + dist);
+        if (dist > 1.5f)
+        {
+            material.color = new Color(material.color.r, material.color.g, material.color.b, 1.0f);
+        }
+        else
+        {
+            material.color = new Color(material.color.r, material.color.g, material.color.b, 0.0f);
+        }
+        print("Distance to target: " + dist);
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("OnTriggerEnter");
-        GetComponent<MeshRenderer>().enabled=true;
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        Debug.Log("OnTriggerExit");
-        GetComponent<MeshRenderer>().enabled = false;
-    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log("OnTriggerEnter");
+    //    material.color = new Color(material.color.r, material.color.g, material.color.b,0.0f);
+    //}
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    Debug.Log("OnTriggerExit");
+    //    material.color = new Color(material.color.r, material.color.g, material.color.b, 1.0f);
+    //}
 }
